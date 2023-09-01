ee84ae5cbe8c2ba7931f77d0c295fb8da79dd6e8
nzgwynn <nzgwynn@gmail.com>
2023-09-01 14:38:55 +0100

    Update name in testthat directory

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(goldilocks)
+library(goldilocksTwoArm)
 
-test_check("goldilocks")
+test_check("goldilocksTwoArm")
